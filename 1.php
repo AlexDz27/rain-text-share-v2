734ca7f4b5abe2f6f6796e54160278725ae0e4ee
Alex Dzyuba <alexeydzyuba27@gmail.com>
2026-04-27 01:24:43 +0300

    introduce returnLocation

--- 1.php
+++ 1.php
@@ -3,6 +3,10 @@
 $fileNumber = pathinfo(basename(__FILE__), PATHINFO_FILENAME);  // [AI: pathinfo]
 require 'main.php';
 
+// [AI]
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https://" : "http://";
+$current_url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,6 +21,7 @@ require 'main.php';
 <main class="main cont">
   <form action="back-end/save.php" method="POST">
     <input name="fileNumber" type="hidden" value="<?= $fileNumber ?>">
+    <input name="returnLocation" type="hidden" value="<?= $current_url ?>">
     <textarea name="textarea" class="textarea"><?= $previousContent ?></textarea>
     <button class="btn btn-save" type="submit">Save</button>
   </form>
--- back-end/save.php
+++ back-end/save.php
@@ -2,4 +2,4 @@
 
 file_put_contents('data/' . $_POST['fileNumber'] . '.txt', $_POST['textarea']);
 
-header('Location: /' . $_POST['fileNumber'] . '.php');
+header('Location: ' . $_POST['returnLocation']);
